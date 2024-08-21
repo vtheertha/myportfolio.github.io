2967719dd82b589f2685bb7e9d14ab2c0a5ab761
vtheertha <137624254+vtheertha@users.noreply.github.com>
2024-08-21 10:15:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+    document.getElementById(sectionId).classList.add('active');
+}
+
+// Initially show the "Academics" section
+document.getElementById('about').classList.add('active');
+
